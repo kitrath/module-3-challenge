0c6775016686b6375b39d8826d7304acbb6b7a93
kitrath <crathmel@gmail.com>
2022-12-19 16:01:06 -0600

    add an initial, working pwd generator

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 // Assignment code here
 
+// TODO: Generate a simple password of given length and test it
+
+// Passwords will be generated from user-chosen combinations
+// of charSets properties.
+const charSets = {
+  upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lower: "abcdefghijklmnopqrstuvwxyz",
+  numeric: "0123456789",
+  // List of special characters obtained
+  // from https://owasp.org/www-community/password-special-characters
+  //
+  // " (quote), ' (single-quote), \ (backslash), & (ampersand) all need
+  // to be escaped with a single backslash
+  special: " !\"#$%\&\'()*+,-./:;<=>?@[\\]^_`{|}~"
+};
+
+const comboSet = charSets.upper + charSets.lower + charSets.numeric + charSets.special;
+
+function generateRandomString(length, config = null) {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += comboSet[Math.floor(Math.random() * comboSet.length)];  
+  }
+  return result;
+}
+
+// TODO: Remove -- Just for intial tests
+const generatePassword = function() {
+  return generateRandomString(50);
+};
+
+/**************** Preserve code below ***************************/
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
