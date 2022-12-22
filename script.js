e593f8a402fd23c06c9a0708a3a497a7ab9549ff
kitrath <crathmel@gmail.com>
2022-12-22 00:42:33 -0600

    fix type for mdn reference

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function buildArrayOfInts(length) {
   return arr;
 }
 
-i// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
