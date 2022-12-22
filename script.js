66888f148b9d07bc602a1b9d6ca94c2bbc75381b
kitrath <crathmel@gmail.com>
2022-12-22 00:36:47 -0600

    add mdn reference for getRandomInt()

--- script.js
+++ script.js
@@ -93,6 +93,7 @@ function buildArrayOfInts(length) {
   return arr;
 }
 
+i// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
